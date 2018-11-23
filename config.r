7041f5b4277f3e1176722aa81286d0f810fc557a
PTobias <paddytobias@me.com>
2018-11-23 15:13:08 +1100

    update config

--- config.r
+++ config.r
@@ -1,19 +1,5 @@
 # Configurations
 
-email = "[[INSERT YOUR EMAIL ADDRESS]]"
-eRA_name = "[[INSERT YOUR NAME]]"
-report_stakeholder = "[[INSERT REPORT RECIPIENT]]"
-stakeholder_email = "[[INSERT EMAIL ADRESS OF STAKEHOLDER]]"
-member = "[[INSERT INSTITUTION NAME - use acronym if possible]]"
-
-
 ## FOR GOOGLE API CREDENTIALS
 client_id = "[[INSERT CLIENT ID]]"
 client_secret = "[[INSERT CLIENT SECRET]]"
-
-## FOR COPYING TEMPLATE WOKRFLOW
-dest_folder_id = "[[INSERT ID OF FOLDER THAT YOU WANT YOUR REPORT IN]]"
-
-## WHAT TO DO AT EACH COMPLETION
-send_to_stakeholder = [[TRUE/FALSE]] # do you want the program to send each new report to the stakeholder upon creation?
-send_to_you = [[TRUE/FALSE]] # do you want the program to send each new report to you upon creation?
