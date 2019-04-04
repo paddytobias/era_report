769fcc4cffa107b650c70e206084bb193077910c
Ubuntu <ubuntu@factory.novalocal>
2019-04-04 13:41:39 +1100

    update

--- run_report.sh
+++ run_report.sh
@@ -1,7 +1,7 @@
 cd /home/ubuntu/era_report/R/
 echo "Begin report script"  >> ../log.txt
 echo date  >> ../log.txt
-echo "----------"
+echo "----------" >> ../log.txt
 /usr/bin/Rscript era_report.R
 echo date  >> ../log.txt
 echo "Finish report script" >> ../log.txt
